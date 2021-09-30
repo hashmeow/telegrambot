6dfe2cb4f19a4a5d030d5b25d7d56b52322609f3
sentinals <91692923+sentinals@users.noreply.github.com>
2021-10-01 01:02:35 +0530

    Hash File for computing hash of each messages

---
+++ hash_md5.py
@@ -0,0 +1,12 @@
+class Hash:
+  def md5checksum(fname):
+
+      md5 = hashlib.md5()
+
+      # handle content in binary form
+      f = open(fname, "rb")
+
+      while chunk := f.read(4096):
+          md5.update(chunk)
+
+      return md5.hexdigest()
